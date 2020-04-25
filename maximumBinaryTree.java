6030dcc4661c046ec8da9ecd5db070ff93823869
Anchit Pandey <40236708+AnchitPandey@users.noreply.github.com>
2020-04-25 10:40:00 -0700

    Create maximumBinaryTree.java

---
+++ maximumBinaryTree.java
@@ -0,0 +1,70 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class maximumBinaryTree {
+    
+    public void quick (TreeNode par, int[] nums, int lowerIndex, int upperIndex, char ch)
+    {
+        // System.out.println("####################################");                                 System.out.println("lower index is "+ lowerIndex);
+        // System.out.println("upper index is "+ upperIndex);
+      //  System.out.println()     
+       
+        if (lowerIndex >= upperIndex)
+        {
+         //   System.out.println("breaking");
+            return;
+        }
+            int max = Integer.MIN_VALUE;
+        int curr = -1;
+        // finding max element
+        for (int i = lowerIndex; i< upperIndex;i+=1)
+        {
+            if (nums[i] > max)
+            {
+                max = nums[i];
+                curr = i;
+            }             
+         }
+        // System.out.println("max element is "+ max);
+        // System.out.println("max element at index "+ curr);
+        TreeNode child = new TreeNode (max);
+        if (ch == 'l')
+            par.left = child;
+        else if (ch =='r')
+            par.right = child; 
+        // call recursively for left
+        quick (child, nums,lowerIndex, curr,'l');
+         // call recursively for right
+        quick (child, nums,curr+1, upperIndex, 'r');
+    }
+    
+    public TreeNode constructMaximumBinaryTree(int[] nums) {
+        
+    if (nums.length ==0)
+        return null;
+    int max = Integer.MIN_VALUE;
+    int curr=  -1;
+        
+    for (int i= 0 ; i< nums.length;i+=1)
+    {
+        if (nums[i] > max)
+        {
+            max = nums[i];
+            curr = i;
+        }
+    }  
+    // System.out.println("current max is at index "+ curr);
+    // System.out.println("current max is"+ max);
+        
+    TreeNode root = new TreeNode (max);
+    quick (root, nums, 0, curr, 'l');
+    quick(root, nums, curr+1, nums.length, 'r');
+    return root;
+    }
+}
