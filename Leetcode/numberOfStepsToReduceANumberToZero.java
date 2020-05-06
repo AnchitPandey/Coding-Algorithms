a06c5dbf560c96058c21c882706c4631593e9e03
Anchit Pandey <40236708+AnchitPandey@users.noreply.github.com>
2020-05-06 11:51:49 -0700

    Create numberOfStepsToReduceANumberToZero.java

---
+++ Leetcode/numberOfStepsToReduceANumberToZero.java
@@ -0,0 +1,14 @@
+class numberOfStepsToReduceANumberToZero {
+    public int numberOfSteps (int num) {
+        int counter =0;
+        while (num > 0)
+        {
+            counter+=1;
+            if (num % 2 ==0)
+                num /=2;
+            else
+                num-=1;
+        }
+        return counter;
+    }
+}
