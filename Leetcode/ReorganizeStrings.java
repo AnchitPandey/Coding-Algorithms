3fc1a2e5ec671473950b2a760fc22d9ff79c7a81
Anchit Pandey <40236708+AnchitPandey@users.noreply.github.com>
2020-12-27 17:20:02 -0800

    https://leetcode.com/problems/reorganize-string/solution/

--- Leetcode/ReorganizeStrings.java
+++ Leetcode/ReorganizeStrings.java
@@ -1,75 +1,68 @@
-class custom implements Comparator <ArrayList> {
-    
-    public int compare (ArrayList o1, ArrayList o2)
+class custom implements Comparator<Model>
+{
+    public int compare (Model m1, Model m2)
     {
-        return -((int)o1.get(1) - (int)o2.get(1));
+        return m2.fre - m1.fre;
     }
 }
 
+class Model {
 
+    char ch;
+    int fre;
+    public Model (char ch, int fre)
+    {
+        this.ch = ch;
+        this.fre = fre;
+    }
+}
 class Solution {
-    public String reorganizeString(String s) {
 
-        HashMap<Character,Integer> mapper = new HashMap<Character, Integer>();
-        
-        for (int i =0 ; i< s.length();i+=1)
-        {
-            char ch = s.charAt(i);
+    // TC: O (N * log N_DIST (<=26) ) =>  O (N)
+    // SC: O (N)
+    
+    public String reorganizeString(String S) {
+        // Create Map
+        HashMap<Character, Integer> mapper = new HashMap<Character, Integer>();
+        for (int i = 0 ; i< S.length();i+=1){       
+            char ch = S.charAt(i);
             mapper.put (ch, mapper.getOrDefault(ch,0)+1);
         }
-        PriorityQueue<ArrayList> pq = new PriorityQueue<ArrayList>(new custom());
-        ArrayList<Character> chs = new ArrayList<Character>(mapper.keySet());
+        // Creating PQ
+        PriorityQueue<Model> pq = new PriorityQueue<Model>(new custom());
+        for (Map.Entry<Character,Integer> me: mapper.entrySet()){
         
-        for (int i=0; i < chs.size();i+=1)
-        {
-            ArrayList putter=  new ArrayList();
-            putter.add (chs.get(i));
-            putter.add (mapper.get(chs.get(i)));
-            pq.add (putter);
-        }       
+            Model m =new Model (me.getKey(), me.getValue());
+            pq.add (m);
+        }
+        // Main Logic
+        StringBuilder sb=new StringBuilder();
+        while (!pq.isEmpty()){
         
-        
-        int len = s.length();
-        StringBuilder sb  = new StringBuilder();
-        char prev = '-';
-        while (sb.length() != s.length())
-        {
-            //System.out.println("in here");
-            ArrayList topmost= pq.poll();
-            //System.out.println("array list is "+ topmost);
-            char ch = (char) topmost.get(0);
-            if (ch != prev)
+            char prev = sb.length () > 0 ? sb.charAt(sb.length()-1) : '-' ;
+            Model ml = pq.poll();
+            if (ml.ch  == prev)
             {
-                sb.append (ch);
-                prev = ch;
-                int fre = (int)topmost.get(1);
-                topmost.remove (1);
-                if (fre > 1)
+                if (pq.size() ==0)
+                    return "";
+                else
                 {
-                    topmost.add (fre-1);
-                    pq.add (topmost);
+                    Model tt = pq.poll();
+                    sb.append (tt.ch);
+                    tt.fre -=1;
+                    if (tt.fre >0)
+                        pq.add (tt);
+                    pq.add (ml);
                 }
             }
             else
             {
-                if (pq.isEmpty())
-                    return "";
-                else
-                {
-                    ArrayList secondmst = pq.poll();
-                    sb.append ((char)secondmst.get(0));
-                    prev = (char)secondmst.get(0);
-                    int fre2 = (int)secondmst.get(1);
-                    if (fre2 > 1)
-                    {
-                        secondmst.remove (1);
-                        secondmst.add (fre2-1);
-                        pq.add (secondmst);
-                     }
-                    pq.add (topmost);
-                }
+                sb.append  (ml.ch);
+                ml.fre-=1;
+                if (ml.fre > 0)
+                    pq.add (ml);           
             }    
-        }
-        return sb.toString ();
+        } 
+        return sb.toString();        
     }
 }
